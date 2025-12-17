2f4894338a65d735df583688612f9b515fd146c3
lukkarj <98267583+lukkarj@users.noreply.github.com>
2025-12-17 16:59:07 -0300

    Update __init__.py

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,4 @@
-from app.schemas.CACertificatesSchema import CACertificateStruct, CACertificateList, GenerateCACertificate, CACertificateDelete, CADeleteResult
+from app.schemas.caCertificatesSchema import CACertificateStruct, CACertificateList, GenerateCACertificate, CACertificateDelete, CADeleteResult
 from app.schemas.certificatesSchema import CertificateFetch, CertificateStruct, CertificateList, GenerateCert, CertificateDelete, DeleteResult, CertificateRenewal, CertificateFilter, CertificateSearch
 from app.schemas.errorSchema import Error
 from app.schemas.requestSchema import RequestSubject, RequestDecodeIn, RequestDecodeOut, RequestOutput
